37f3194cbd976d1a990d6ef321716509746596a0
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-09-24 15:51:55 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Juan Ceballos on 9/24/19.
+//  Copyright © 2019 Juan Ceballos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
